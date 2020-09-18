9a79b0de18d07e7dd40818311882bdeab531156a
acheinu1 <acheinue@gmail.com>
2020-09-18 21:42:36 +0100

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,51 @@
+updateMap = () => {
+  fetch('/assets/geojson/data.json')
+    .then((response) => response.json())
+    .then((rsp) => {
+      console.log(rsp.features);
+      rsp.features.forEach((element) => {
+        longitude = element.geometry.coordinates[0];
+        latitude = element.geometry.coordinates[1];
+
+        // to change markers colours based on payment status================= line 11-17
+        status = element.properties.status;
+        if(status==="not paid"){
+            color="red";
+        }
+        else{
+            color="green"
+        }
+        // to change markers colours based on payment status===================
+        // mark on the map
+        new mapboxgl.Marker({
+            draggable:false,
+            color:"red",
+        })
+        .setLngLat([longitude, latitude])
+        .addTo(map);
+      });
+    });
+};
+
+updateMap();
+
+//  mapbox cdn===================================================
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiYWNoZWludWUiLCJhIjoiY2tmM25yaXp2MDR6dDJybm9qcTZyMHY2MiJ9.6v7PIIvPk96ZuH1nrNmsEA';
+    // Set bounds to New York, New York
+var bounds = [
+    [-712.7744293212891, 9.114300517448383], // Southwest coordinates
+    [ -712.331714630127, 9.412822817158808] // Northeast coordinates
+    ];
+     
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+//   zoom:6,
+  center: [-712.6604461669922, 9.152265159854796],
+zoom: 15,
+
+});
+//  mapbox cdn===================================================
